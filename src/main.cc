2c1b4e8213dca1375d770d89bf4a54713f08afb2
u32 <u32@posteo.net>
2025-04-05 13:47:45 +0200

    wrap packet capture in a class

--- src/main.cc
+++ src/main.cc
@@ -1,40 +1,19 @@
-#include <soci/soci.h>
-#include <soci/sqlite3/soci-sqlite3.h>
-#include <thread>
-#include <unistd.h>
-
-#include "packet_stream.hpp"
+#include "packet_capture.hpp"
 #include "gui/gui.hpp"
 
 #include "appstate.hpp"
 
 AppState APP_STATE;
 
-int fetch_packets(int cancelfd)
-{
-    auto stream = PacketStream::create(cancelfd);
-
-    while (auto optnxt = stream->next()) {
-        std::lock_guard<std::mutex> lck { APP_STATE.mutex };
-        APP_STATE.packet_store.push_back(std::move(*optnxt));
-    }
-
-    return 0;
-}
-
 int main() 
 {
+    PacketCapture capture {};
     gui::init();
 
-    /// TODO: wrap in some class?
-    int cancelfds[2];
-    pipe(cancelfds);
-
-    std::thread packet_fetcher(fetch_packets, cancelfds[0]);
+    capture.start();
 
     gui::launch();
 
-    write(cancelfds[1], "#", 1);
-    packet_fetcher.join();
+    capture.stop();
     return 0;
 }
